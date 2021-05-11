cd805451b51fe62e54c8cd8215c41d9592caf02c
HaHe-a <54789194+HaHe-a@users.noreply.github.com>
2021-05-11 18:37:09 +0800

    Add files via upload

---
+++ 树状数组维护逆序对.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int T[100005];
+int n;
+int lowbit(int x)
+{
+    return x&(-x);
+}
+void add(int pos,int x)
+{
+    for(int i=pos;i<=n;i+=lowbit(i)){
+        T[i]+=x;
+    }
+}
+int getsum(int pos)
+{
+    int ans=0;
+    for(int i=pos;i>0;i-=lowbit(i)){
+        ans+=T[i];
+    }
+    return ans;
+}
+int a[100005];
+int main()
+{
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)scanf("%d",&a[i]);
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        add(a[i],1);
+        cout<<i-getsum(a[i])<<endl;
+        ans+=i-getsum(a[i]);
+    }
+    cout<<ans<<endl;
+}
